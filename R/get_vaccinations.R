629790c76cd418594dab33c95cce76a3084ec2f1
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2021-11-19 18:16:36 -0500

    Using single lookup table for OWID location renamings

--- R/get_vaccinations.R
+++ R/get_vaccinations.R
@@ -3,35 +3,21 @@
 #' @title get_vax
 #' @description Get vaccinations from OWID
 #'
-
-#'
 #' @export
 #'
 
 get_vax <- function() {
-  df <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv",
-    as.is = TRUE,
+  df <- fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv",
     stringsAsFactors = FALSE,
     check.names = FALSE
   ) %>%
     mutate(date = as.Date(date)) %>%
     mutate(iso_code = recode(iso_code, "OWID_KOS" = "XKX")) %>%
     filter(!grepl("OWID", iso_code)) %>%
-    mutate(location = recode(location,
-      "United Kingdom"            = "The United Kingdom",
-      "Syria"                     = "Syrian Arab Republic",
-      "South Korea"               = "Republic of Korea",
-      "Sint Maarten (Dutch part)" = "Sint Maarten",
-      "Russia"                    = "Russian Federation",
-      "Pitcairn"                  = "Pitcairn Islands",
-      "Moldova"                   = "Republic of Moldova",
-      "Macao"                     = "Macau",
-      "Laos"                      = "Lao People's Democratic Republic",
-      "Iran"                      = "Iran (Islamic Republic of)",
-      "Curacao"                   = "Curaçao",
-      "Cape Verde"                = "Cabo Verde"
-    )) %>%
+    mutate(location = recode(location, !!!owid_lk)) %>%
     mutate(daily_vaccinations_per_hundred = daily_vaccinations_per_million / 10000)
+
+  return(df)
 }
 
 
@@ -46,26 +32,14 @@ get_vax <- function() {
 #'
 
 get_vax_manufacturers <- function() {
-  df <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/locations.csv",
-    as.is = TRUE,
+  df <- fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/locations.csv",
     stringsAsFactors = FALSE,
     check.names = FALSE
   ) %>%
     mutate(last_observation_date = as.Date(last_observation_date)) %>%
     mutate(iso_code = recode(iso_code, "OWID_KOS" = "XKX")) %>%
     filter(!grepl("OWID", iso_code)) %>%
-    mutate(location = recode(location,
-      "United Kingdom"            = "The United Kingdom",
-      "Syria"                     = "Syrian Arab Republic",
-      "South Korea"               = "Republic of Korea",
-      "Sint Maarten (Dutch part)" = "Sint Maarten",
-      "Russia"                    = "Russian Federation",
-      "Pitcairn"                  = "Pitcairn Islands",
-      "Moldova"                   = "Republic of Moldova",
-      "Macao"                     = "Macau",
-      "Laos"                      = "Lao People's Democratic Republic",
-      "Iran"                      = "Iran (Islamic Republic of)",
-      "Curacao"                   = "Curaçao",
-      "Cape Verde"                = "Cabo Verde"
-    ))
+    mutate(location = recode(location, !!!owid_lk))
+  
+  return(df)
 }
