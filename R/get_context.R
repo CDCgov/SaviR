57c4cf4d8f05a346fab088f1ebbad73e4d0a0f32
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2021-12-21 16:07:26 -0500

    Updating get_gdeltnews - Adding data documentation - Starting date at d-1 to avoid failure - Explictly testing for number of columns - Reducing lookback period for testing purposes to speed up CI/CD

--- R/get_context.R
+++ R/get_context.R
@@ -1,20 +1,45 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #' @title get_gdeltnews
-#' @description Get last 2 weeks of GDELT COVID news. Deduplicated by DateTime, URL, and DomainCountryCode.
-
+#' @description
+#' Get last 2 weeks of GDELT COVID news. Deduplicated by DateTime, URL, and DomainCountryCode.
+#'
+#' @param period (numeric) Number of days to look back from start_date
+#' @param start_date (Date) Date from which to look back
+#'
+#' @return A data frame with n rows and 17 variables:
+#' \itemize{
+#'   \item{\code{DateTime}}{  POSIXct}
+#'   \item{\code{URL}}{  character}
+#'   \item{\code{Title}}{  character}
+#'   \item{\code{SharingImage}}{  character}
+#'   \item{\code{LangCode}}{  character}
+#'   \item{\code{DocTone}}{  double}
+#'   \item{\code{DomainCountryCode}}{  character}
+#'   \item{\code{Location}}{  character}
+#'   \item{\code{Lat}}{  double}
+#'   \item{\code{Lon}}{  double}
+#'   \item{\code{CountryCode}}{  character}
+#'   \item{\code{Adm1Code}}{  character}
+#'   \item{\code{GeoType}}{  character}
+#'   \item{\code{ContextualText}}{  character}
+#'   \item{\code{GeoCoord}}{  character}
+#'   \item{\code{Adm2Code}}{  character}
+#' }
+#'
+#' @section Note:
+#' Function may fail if start date is set to current date or in future. News data files aren't often avilable for the current date.
 #'
 #' @export
-#'
 #' @examples
 #' \dontrun{
 #' gdeltnews_df <- get_gdeltnews()
 #' }
 #'
-get_gdeltnews <- function(period = 14) {
+get_gdeltnews <- function(period = 14, start_date = Sys.Date() - 1) {
   gdeltnews <- data.frame()
 
-  for (d in seq(Sys.Date() - period, Sys.Date(), by = "day")) {
+  for (d in seq(start_date - period, start_date, by = "day")) {
     message(zoo::as.Date(d))
     tryCatch(
       container <- jsonlite::stream_in(
@@ -36,8 +61,9 @@ get_gdeltnews <- function(period = 14) {
     gc()
   }
 
-  return(gdeltnews %>%
-    mutate(Date = sub("T.*", "", DateTime)) %>%
-    # select(-c(DateTime, URL, SharingImage, DomainCountryCode)) %>%
-    unique())
+  out <- gdeltnews %>%
+    mutate(DateTime = as.POSIXct(DateTime)) %>%
+    distinct()
+
+  return(out)
 }
