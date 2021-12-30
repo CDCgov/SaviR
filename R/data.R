de368895b1c470cec317e8b6b9cdf064eb1fa918
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2021-12-30 13:36:23 -0500

    Updating docs

--- R/data.R
+++ R/data.R
@@ -27,7 +27,7 @@
 
 #' @title OWID Testing Metadata
 #' @describeIn get_owid_testing_meta Saved dataset
-#' @description A metadata table used within \code{\link[SaviR::get_testing]{get_testing()}} to determine how country testing data is computed.
+#' @description A metadata table used within [get_testing()] to determine how country testing data is computed.
 #' @format A data frame with 141 rows and 5 variables:
 #' \itemize{
 #'   \item{\code{iso3code}}{  character ISO 3166-1 alpha-3 country code}
