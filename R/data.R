9a88c3d26c574ad39cf3575c9c0beef5f9f69ed7
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2022-08-11 10:39:56 -0400

    Updating CIA WFB countries documentation

--- R/data.R
+++ R/data.R
@@ -66,7 +66,7 @@ onetable_addn_countries <- data.frame(
 #' Countries with no current UNWPP data
 #' that are manually updated here from CIA world factbook
 #' https://www.cia.gov/the-world-factbook/field/population/country-comparison
-#' Currently 2022 projections are being used
+#' Currently 2022 estimates are being used
 cia_wfb_addn_countries <- data.frame(
   country = c("Pitcairn Islands", "Kosovo"),
   id = c("PCN", "XKX"),
