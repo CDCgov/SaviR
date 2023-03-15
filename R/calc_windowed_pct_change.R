f44769cff80129a6bb903fdc2cc3ceceea369a49
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-03-15 14:21:08 -0400

    updating signature - Not handling grouped case internally, which is more transparent

--- R/calc_windowed_pct_change.R
+++ R/calc_windowed_pct_change.R
@@ -9,51 +9,42 @@
 #' if data are not complete for every date.
 #' @param df Dataframe with at least [date, new_cases] columns
 #' @param window (default: 14) number of days the comparison windows should be
-#' @param grp optional grouping variable (such as isocode, country name, region, etc.)
+#' @param return_totals (default: FALSE) return running sums used to compute `pct_change`?
 #' 
-#' @return a df summarized by date with new column `pct_change`, of dim [n, 2 (or 3, if `grp` is specified)]
+#' @return a df summarized by date with new column `pct_change`, or [pct_change, cases_current_{window}, cases_prev_{window}] if `return_totals` is `TRUE`
 #' @importFrom RcppRoll roll_sum
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' data <- get_covid_df()
-#' calc_window_pct_change(data, window = 14, grp = "country")
+#' calc_window_pct_change(data, window = 14)
+#' # For grouped operations, group data beforehand and pipe:
+#' data |>
+#'   group_by(iso2code, country) |>
+#'   calc_window_pct_change(data, window = 14)
 #' }
 #'
-calc_window_pct_change <- function(df, window = 14L, grp = NULL) {
+calc_window_pct_change <- function(df, window = 14L, return_totals = FALSE) {
   # Assert that we have the cols we need
   stopifnot(all(c("date", "new_cases") %in% colnames(df)))
 
-  # Grouped operation
-  # (probably a much cleaner way to do this to avoid a large branch, but w/e)
-  if (!missing(grp)) {
-    grp_quo <- rlang::enquo(grp)
-    
-    out <- df |>
-      group_by({{ grp_quo }}, date) |>
-      summarize(new_cases = sum(new_cases, na.rm = TRUE)) |>
-      arrange(date) |>
-      group_by({{ grp_quo }}) |>
-      mutate(
-        pct_change = (
-          roll_sum(new_cases, n = window, align = "right", fill = NA) /
-          roll_sum(dplyr::lag(new_cases, n = window), n = window, align = "right", fill = NA)
-        )
-      ) |>
-      ungroup()
-  } else {
-    out <- df |>
-      group_by(date) |>
-      summarize(new_cases = sum(new_cases, na.rm = TRUE)) |>
-      ungroup() |>
-      arrange(date) |>
-      mutate(
-        pct_change = (
-          roll_sum(new_cases, n = window, align = "right", fill = NA) /
-          roll_sum(dplyr::lag(new_cases, n = window), n = window, align = "right", fill = NA)
-        )
-      )
+  current_col <- as.name(sprintf("cases_current_%d", window))
+  prev_col <- as.name(sprintf("cases_prev_%d", window))
+
+  out <- df |>
+    group_by(date, .add = TRUE) |>
+    summarize(new_cases = sum(new_cases, na.rm = TRUE), .groups = "drop_last") |>
+    arrange(date) |>
+    mutate(
+      !!current_col := roll_sum(new_cases, n = window, align = "right", fill = NA),
+      !!prev_col := roll_sum(dplyr::lag(new_cases, n = window), n = window, align = "right", fill = NA),
+      pct_change = !!current_col / !!prev_col
+    ) |>
+    ungroup()
+
+  if (!return_totals) {
+    out <- select(out, -!!prev_col, -!!current_col)
   }
 
   return(out)
--- tests/testthat/test_window_calc.R
+++ tests/testthat/test_window_calc.R
@@ -1,4 +1,4 @@
-test_that("Window Calculation works as expected", {
+test_that("Window Calculation works for ungrouped data", {
   a <- tibble(
     my_var = rep(c("a", "b"), each = 49),
     new_cases = rep(rep(1:7, each = 7), 2)
@@ -11,19 +11,41 @@ test_that("Window Calculation works as expected", {
   # to half-size by date
   b <- calc_window_pct_change(a, 7)
 
-  expect_identical(dim(b), c(49, 3))
+  expect_identical(dim(b), c(49L, 3L))
 
-  # Grouped operations work
-  # and return full-sized data
-  d <- calc_window_pct_change(a, 7, "my_var")
+  # If we ask to return totals, that should be included
+  d <- calc_window_pct_change(a, window = 7, return_totals = TRUE)
+  
+  expect_identical(dim(d), c(49L, 5L))
+  expect_true(all(c("cases_current_7", "cases_prev_7") %in% colnames(d)))
+})
 
-  expect_identical(dim(d), c(98, 4))
+test_that("Window calculation works for grouped data", {
+  a <- tibble(
+    my_var = rep(c("a", "b"), each = 49),
+    new_cases = rep(rep(1:7, each = 7), 2)
+  ) |>
+    group_by(my_var) |>
+    mutate(date = row_number()) |>
+    ungroup()
 
-  # unquoted names should work as well
-  # (but aren't safe due to symbol collision)
-  e <- calc_window_pct_change(a, 14, my_var)
+  # Under the pre-grouped case:
+  # - should be full sized still
+  # - grouped var should still be in place
+  b <- a |>
+    group_by(my_var) |>
+    calc_window_pct_change(window = 14)
+  
+  expect_identical(dim(b), c(98L, 4L))
+  expect_true("my_var" %in% colnames(b))
 
-  expect_identical(dim(b), c(98, 4))
-  expect_identical(d, e)
+  # If we ask to return totals, that should be included
+  d <- a |>
+    group_by(my_var) |>
+    calc_window_pct_change(window = 14, return_totals = TRUE)
+  
+  expect_identical(dim(d), c(98L, 6L))
+  expect_true("my_var" %in% colnames(d))
+  expect_true(all(c("cases_current_14", "cases_prev_14") %in% colnames(d)))
 
 })
